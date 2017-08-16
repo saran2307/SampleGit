6d416206f26448e084a823b8841291511969a707
saran2307 <saran.nghz@gmail.com>
2017-08-16 14:15:11 +0530

    Initial version

--- app/src/main/java/sg/samplegit/MainActivity.java
+++ app/src/main/java/sg/samplegit/MainActivity.java
@@ -17,8 +17,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        // comment added 
+       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
